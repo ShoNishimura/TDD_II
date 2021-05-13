60bb6f70c23ce99b44327ecfc62ea95b140ac723
shonishimura <shonishimura.dev@gmail.com>
2021-05-13 20:26:13 +0900

    testMethodの動的な呼び出し

--- xunit.py
+++ xunit.py
@@ -6,15 +6,21 @@
 # 複数のテストを走らせる
 # 収集したテスト結果を出力する
 
-# テストメソッドが呼ばれたらtrueを出力し、
-# 呼ばれなかったらfalseを出力する
+class TestCase:
+    pass
+
 class WasRun:
     def __init__(self, name):
         self.wasRun = None
+        self.name = name
     def run(self):
-        self.testMethod()
+        method = getattr(self, self.name)
+        method()
     def testMethod(self):
         self.wasRun = 1
+
+# テストメソッドが呼ばれたらtrueを出力し、
+# 呼ばれなかったらfalseを出力する
 
 test =  WasRun("testMethod")
 print(test.wasRun) #false
