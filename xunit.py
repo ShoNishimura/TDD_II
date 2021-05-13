3a8b9c61bdebd03b56bed32bf88e0a97c41d5028
shonishimura <shonishimura.dev@gmail.com>
2021-05-13 20:08:40 +0900

    runメソッドを実装

--- xunit.py
+++ xunit.py
@@ -11,11 +11,13 @@
 class WasRun:
     def __init__(self, name):
         self.wasRun = None
+    def run(self):
+        self.testMethod()
     def testMethod(self):
         self.wasRun = 1
 
 test =  WasRun("testMethod")
 print(test.wasRun) #false
-test.testMethod()
+test.run()
 print(test.wasRun) #true
 
