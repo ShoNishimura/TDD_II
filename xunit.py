3626d179454b1d71726ed3b736ddacce21fedbbd
shonishimura <shonishimura.dev@gmail.com>
2021-05-17 21:21:52 +0900

    tearDownテストを追加

--- xunit.py
+++ xunit.py
@@ -11,16 +11,21 @@ class TestCase:
         self.name = name  
     def setUp(self):
         pass
+    def tearDown(self):
+        pass
     def run(self):
         self.setUp()
         method = getattr(self, self.name)
         method()  
+        self.tearDown()
 
 class WasRun(TestCase):
     def setUp(self):
         self.log = "setUp "
     def testMethod(self):
         self.log = self.log + "testMethod "
+    def tearDown(self):
+        self.log = self.log + "tearDown "
     
 # テストメソッドが呼ばれたらtrueを出力し、
 # 呼ばれなかったらfalseを出力する
@@ -28,7 +33,7 @@ class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
-        assert("setUp testMethod " == test.log)
+        assert("setUp testMethod tearDown " == test.log)
 
 TestCaseTest("testTemplateMethod").run()
 
