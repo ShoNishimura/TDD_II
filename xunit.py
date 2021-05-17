70ebacd060cfc062a6bb2f07becabe988975f32b
shonishimura <shonishimura.dev@gmail.com>
2021-05-17 19:44:17 +0900

    setUpメソッドを追加

--- xunit.py
+++ xunit.py
@@ -9,25 +9,37 @@
 class TestCase:
     def __init__(self, name):
         self.name = name  
+    def setUp(self):
+        pass
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()  
-        
+
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
         super().__init__(name)
-
-
+    def setUp(self):
+        self.wasSetUp = 1
     def testMethod(self):
         self.wasRun = 1
-
-
+    
+    
 # テストメソッドが呼ばれたらtrueを出力し、
 # 呼ばれなかったらfalseを出力する
+class TestCaseTest(TestCase):
+    
+    def testRunning(self):
+        test =  WasRun("testMethod")
+        assert(not test.wasRun) #false
+        test.run()
+        assert(test.wasRun) #true
+    def testSetUp(Self):
+        test = WasRun("testMethod")
+        test.run()
+        assert(test.wasSetUp)
 
-test =  WasRun("testMethod")
-print(test.wasRun) #false
-test.run()
-print(test.wasRun) #true
+TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
 
